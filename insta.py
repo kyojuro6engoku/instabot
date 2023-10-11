6fa59112fa453ff90be5e3ef5ae6492c14f452d5
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-10-11 17:29:02 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -7,11 +7,11 @@ bot_token = "6390496152:AAElWXKiIeMSYJZ4InTVAexWJOkqeraEenA"
 
 bot = Client("my_bot", api_id, api_hash, bot_token=bot_token)
 
-@bot.on_message(filters.command("start"))
+@Client.on_message(filters.command("start"))
 async def start(client, message):
     await message.reply_text("Welcome to the Instagram Reel Downloader bot! Send me an Instagram Reel link, and I'll download it for you.")
 
-@bot.on_message(filters.text & ~filters.command)
+@Client.on_message(filters.text & ~filters.command)
 async def download_reels(client, message):
     # Check if the message contains a link
     if "https://www.instagram.com/reel/" in message.text:
