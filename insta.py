56ae8d8782cc360986128813d8436624bae7769c
Amal Nath <134857584+kyojuro6engoku@users.noreply.github.com>
2023-10-12 08:22:07 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -11,7 +11,7 @@ bot = Client("my_bot", api_id, api_hash, bot_token=bot_token)
 async def start(client, message):
     await message.reply_text("Welcome to the Instagram Reel Downloader bot! Send me an Instagram Reel link, and I'll download it for you.")
 
-@Client.on_message(filters.text & ~filters.command)
+@Client.on_message(filters.text & filters.command)
 def your_message_handler(client, message):
     # Your message handling code here
 
